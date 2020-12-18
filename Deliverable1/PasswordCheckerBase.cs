4323aaf0d06e7420159f8fea43a4944e097a8ac1
Wendi <wendimagee@gmail.com>
2020-12-18 16:03:23 -0500

    Add project files.

---
+++ Deliverable1/PasswordCheckerBase.cs
@@ -0,0 +1,7 @@
+﻿namespace Deliverable1
+{
+    public abstract static class PasswordCheckerBase
+    {
+        public abstract static string CheckPassword(string password);
+    }
+}
---
+++ Deliverable1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace Deliverable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a password that follows these rules:");
+            Console.WriteLine("    Must have at least one uppercase letter");
+            Console.WriteLine("    Must have at least one lowercase letter");
+            Console.WriteLine("    Must be a minimum of 7 characters");
+            Console.WriteLine("    Must be a maximum of 12 characters");
+            Console.WriteLine("    Must include an exclamation point (!)");
+            string password = Console.ReadLine();
+        }
+    }
+}
