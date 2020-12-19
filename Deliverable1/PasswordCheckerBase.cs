890ea482fc8660794f8342d692a57655d175f2a9
Wendi <wendimagee@gmail.com>
2020-12-18 21:04:46 -0500

    Deleting unnecessary docs, finishing build

--- Deliverable1/PasswordCheckerBase.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Deliverable1
-{
-    public abstract static class PasswordCheckerBase
-    {
-        public abstract static string CheckPassword(string password);
-    }
-}
--- Deliverable1/Program.cs
+++ Deliverable1/Program.cs
@@ -1,25 +1,45 @@
 ﻿using System;
 using System.Linq;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-
-
 
 
 namespace Deliverable1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Please enter a password that follows these rules:");
-            Console.WriteLine("    Must have at least one uppercase letter");
-            Console.WriteLine("    Must have at least one lowercase letter");
-            Console.WriteLine("    Must be a minimum of 7 characters");
-            Console.WriteLine("    Must be a maximum of 12 characters");
-            Console.WriteLine("    Must include an exclamation point (!)");
-            string password = Console.ReadLine();
+        public static void Main(string[] args)
+           {
+              Console.WriteLine("Please enter a password that follows these rules:");
+              Console.WriteLine("    Must have at least one uppercase letter");
+              Console.WriteLine("    Must have at least one lowercase letter");
+              Console.WriteLine("    Must be a minimum of 7 characters");
+              Console.WriteLine("    Must be a maximum of 12 characters");
+              Console.WriteLine("    Must include an exclamation point (!)");
+               string password = Console.ReadLine();
+
+                // we need to list some boolean expressions here for validation
+                bool containsAtLeastOneUppercase = password.Any(char.IsUpper);
+                bool containsAtLeastOneLowercase = password.Any(char.IsLower);
+
+            if (password.Length < 7 || password.Length > 12)
+                {
+                Console.WriteLine("Error");
+                }
+            else if (!password.Any(char.IsUpper))
+                {
+                Console.WriteLine("Error");
+                }
+            else if (!password.Any(char.IsLower))
+                {
+                Console.WriteLine("Error");
+                }
+            else if (!password.Contains("!"))
+                {
+                Console.WriteLine("Error");
+                }
+            else
+                {
+                Console.WriteLine("Password valid and accepted");
+                }
         }
     }
 }
